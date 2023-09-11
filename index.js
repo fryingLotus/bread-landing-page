d967de00075f5c230378b34bace2081768075265
Frying Lotus <blightningboltz5@gmail.com>
2023-09-11 23:03:27 +0700

    finishing menu contact and reserve page

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Select the div where the menu text will be displayed
-const menuText = document.querySelector('#menuText');
-
 // Select all the buttons in the menuContainer section
 const buttons = document.querySelectorAll('.menuContainer .menuButton');
 
@@ -15,24 +12,83 @@ buttons.forEach(function(button) {
     // Change the background color of the clicked button
     button.style.backgroundColor = '#ff5338';
 
-    // Remove all menu-related classes from menuText
-    menuText.classList.remove('dinnerClass', 'lunchClass','brunchClass','drinkClass');
-
-    // Change the text in the menuText div based on the button clicked
-    if (button.textContent === 'DINNER') {
-      menuText.textContent = 'DINNER MENU';
-      menuText.classList.add('dinnerClass');
-    } else if (button.textContent === 'LUNCH') {
-      menuText.textContent = 'LUNCH MENU';
-      menuText.classList.add('lunchClass');
-    } else if (button.textContent === 'BRUNCH')
-    {
-      menuText.textContent = 'BRUNCH MENU';
-      menuText.classList.add('brunchClass');
-    } else if (button.textContent === 'DRINKS')
-    {
-      menuText.textContent = 'DRINK MENU';
-      menuText.classList.add('drinkClass');
-    }
+    // Prepend the Bento menu to the menuContainer
+   
   });
+});
+
+// Add individual event listeners for each button
+document.getElementById('ramenButton').addEventListener('click', function() {
+  var menuContainer = document.querySelector('.menuItem');
+  var ramenMenu = document.getElementById('ramenMenu');
+  menuContainer.prepend(ramenMenu);
+  ramenMenu.classList.add('slideIn');
+  var children = ramenMenu.children;
+  for(var i = 0; i < children.length; i++) {
+    var child = children[i];
+    child.classList.add('animated-item');
+    
+    // Remove the class after the animation ends
+    child.addEventListener('animationend', function() {
+      this.classList.remove('animated-item');
+    });
+  }
+});
+
+document.getElementById('bentoButton').addEventListener('click', function() {
+  var menuContainer = document.querySelector('.menuItem');
+  var bentoMenu = document.getElementById('bentoMenu');
+  menuContainer.prepend(bentoMenu);
+  bentoMenu.classList.add('slideIn');
+  var children = bentoMenu.children;
+  for(var i = 0; i < children.length; i++) {
+    var child = children[i];
+    child.classList.add('animated-item');
+    
+    // Remove the class after the animation ends
+    child.addEventListener('animationend', function() {
+      this.classList.remove('animated-item');
+    });
+  }
+});
+
+document.getElementById('brunchButton').addEventListener('click', function() {
+  var menuContainer = document.querySelector('.menuItem');
+  var brunchMenu = document.getElementById('brunchMenu');
+  menuContainer.prepend(brunchMenu);
+  brunchMenu.classList.add('slideIn');
+  var children = brunchMenu.children;
+  for(var i = 0; i < children.length; i++) {
+    var child = children[i];
+    child.classList.add('animated-item');
+    
+    // Remove the class after the animation ends
+    child.addEventListener('animationend', function() {
+      this.classList.remove('animated-item');
+    });
+  }
+});
+
+document.getElementById('drinkButton').addEventListener('click', function() {
+  var menuContainer = document.querySelector('.menuItem');
+  var drinkMenu = document.getElementById('drinkMenu');
+  menuContainer.prepend(drinkMenu);
+  drinkMenu.classList.add('slideIn');
+  var children = drinkMenu.children;
+  for(var i = 0; i < children.length; i++) {
+    var child = children[i];
+    child.classList.add('animated-item');
+    
+    // Remove the class after the animation ends
+    child.addEventListener('animationend', function() {
+      this.classList.remove('animated-item');
+    });
+  }
+});
+
+// Repeat for other buttons...
+const input = document.querySelector('#birthday');
+input.value = '2023-09-11'; // Set the value
+input.addEventListener('input', () => {
+  console.log(input.value); // Log the value when it changes
 });
